24c6756ee163cf75abf5afaebf4a0db979bf6062
Samuel W. Flint <swflint@flintfam.org>
2021-05-12 17:30:34 -0500

    Switch to the Anatole theme
     - Delete old themes
     - Update social links
     - Add a headshot
     - Move PGP stuff

--- themes/matomo/src/js/promise-polyfill.js
+++
@@ -1,6 +0,0 @@
-const promiseUrl = document
-  .querySelector('script[data-id="promise-polyfill"]')
-  .getAttribute('data-promise-polyfill-url');
-
-if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent))
-  document.write(`<script src="${promiseUrl}"></script>`);
